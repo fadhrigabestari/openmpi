8e0fbacd7cd6208a2788233b91f9da07fdafa81b
Fadhriga Bestari <fadhrigabestari@gmail.com>
2019-04-05 15:18:08 +0700

    Add serial radix sort

--- src/my_radix.c
+++ src/my_radix.c
@@ -26,15 +26,110 @@ void rng(int* arr, int n) {
   }
 }
 
+// get specific bit at index = idx
+int* generateFlag(int* arr, int n, int idx) {
+  int* flag = (int*) malloc(n * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
+    if ((arr[i] >> idx) & 1 == 1) {
+      flag[i] = 1;
+    } else {
+      flag[i] = 0;
+    }
+  }
+  return flag;
+}
+
+
+
+// create I-down array
+int* generateIDown(int* flag, int n) {
+  int* iDown = (int*) malloc(n * sizeof(int));
+  int val = 0;
+
+  iDown[0] = val;
+  for (int i = 1; i < n; i++) {
+    if (flag[i - 1] == 0) {
+      val++;
+    }
+    iDown[i] = val;
+  }
+  return iDown;
+}
+
+// create I-up array
+int* generateIUp(int* flag, int n) {
+  int* iUp = (int*) malloc(n * sizeof(int));
+  int val = n - 1;
+
+  iUp[n - 1] = val;
+  for (int i = n - 2; i >= 0; i--) {
+    if (flag[i + 1] == 1) {
+      val--;
+    }
+    iUp[i] = val;
+  }
+  return iUp;
+}
+
+int* generateShouldIndex(int* flag, int* iDown, int* iUp, int n) {
+  int* shouldIndex = (int*) malloc(n * sizeof(int));
+
+  for (int i = 0; i < n; i++) {
+    if (flag[i] == 0) {
+      shouldIndex[i] = iDown[i];
+    } else {
+      shouldIndex[i] = iUp[i];
+    }
+  }
+  return shouldIndex;
+}
+
+// permute
+void permute(int* arr, int* flag, int* iDown, int* iUp, int n) {
+  int* shouldArr = (int*) malloc(n * sizeof(int));
+
+  int* shouldIndex = generateShouldIndex(flag, iDown, iUp, n);
+
+  for (int i = 0; i < n; i++) {
+    shouldArr[shouldIndex[i]] = arr[i];
+  }
+
+  for (int i = 0; i < n; i++) {
+    arr[i] = shouldArr[i];
+  }
+}
+
+void split(int* arr, int n, int idx) {
+  int* flag = generateFlag(arr, n, idx);
+  int* iDown = generateIDown(flag, n);
+  int* iUp = generateIUp(flag, n);
+
+  permute(arr, flag, iDown, iUp, n);
+}
+
+void radixSort(int* arr, int n) {
+  for (int i = 0; i < 32; i++) {
+    split(arr, n, i);
+  }
+}
+
 int main(int argc, char** argv) {
   int n_element = atoi(argv[1]);
-  int arrlel[9] = {22, 21, 11, 39, 99, 3230, 323, 110, 32123};
   int id, comm_size;
   int* arr = (int*) malloc(n_element * sizeof(int));
+  int* arr_serial = (int*) malloc(n_element * sizeof(int));
 
   rng(arr, n_element);
-  double start, end;
+  rng(arr_serial, n_element);
 
+  double start, end;
+  double start_serial, end_serial;
+  
+  // start serial
+  start_serial = MPI_Wtime();
+  radixSort(arr, n_element);
+  end_serial = MPI_Wtime();
   // start time
   start = MPI_Wtime();
 
@@ -214,10 +309,12 @@ int main(int argc, char** argv) {
   MPI_Finalize();
   end = MPI_Wtime();
 
-  double elapsed_time = (end - start) * 1000000;
-
   if (id == 0) {
-    printf("elapsed time: %f\n", elapsed_time);
+    double elapsed_time = (end - start) * 1000000;
+    double elapsed_time_serial = (end_serial - start_serial) * 1000000;
+
+    printf("elapsed time serial: %f\n", elapsed_time_serial);
+    printf("elapsed time parallel: %f\n", elapsed_time);
     printToFile(arr, n_element, elapsed_time);
   }
 }
